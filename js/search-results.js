bd2624646719fe564b13c788a72ec65e40120c7a
gaston-lm <gastonlm11lm@gmail.com>
2020-06-03 13:29:23 -0300

    buscador (faltan detalles)

---
+++ js/search-results.js
@@ -0,0 +1,117 @@
+window.addEventListener('load', function() {
+
+
+    let queryString = new URLSearchParams(location.search)
+
+    let loBuscado = queryString.get("searcher");
+
+    fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=' + loBuscado)
+    .then(
+        function(respuesta) {
+            return respuesta.json();            
+        }
+    )
+    .then(
+        function(informacion) {
+
+            console.log(informacion)
+
+            arrayTracks = informacion.data;
+
+            for (let i = 0; i < arrayTracks.length; i++) {
+
+                let trackTitle = arrayTracks[i].title;
+
+                let trackExplicit = arrayTracks[i].explicit_lyrics;
+
+                let trackArtist = arrayTracks[i].artist.name;
+
+                let trackAlbum = arrayTracks[i].album.title;
+
+                let duration = arrayTracks[i].duration;
+
+                let trackImg = arrayTracks[i].album.cover_big;
+
+                if (trackExplicit == true) {
+                    console.log("si")
+                    let trackItem =
+                    `
+                    <li class="track-item">
+                        <div class="img-container">
+                            <img class="track-img" src="` + trackImg + `" alt="track-image">
+                        </div>
+                        <a href="" class="fav"><i class="far fa-heart"></i></a>
+                        <a href="" class="track-title">` + trackTitle + `</a>
+                        <div class="info-mobile">
+                            <a href="" class="track">` + trackTitle + `</a>
+                            <a href="" class="artist">` + trackArtist + `</a>
+                        </div>
+                        <p class="explicit">E</p>
+                        <a href="" class="track-artist">` + trackArtist + `</a>
+                        <a href="" class="track-album">` + trackAlbum + `</a>
+                        <p class="duration">` + duration + `</p>
+                    </li>
+                    `
+                    document.querySelector(".ul").innerHTML += trackItem;
+                } else {
+                    let trackItem =
+                    `
+                    <li class="track-item">
+                        <div class="img-container">
+                            <img class="track-img" src="` + trackImg + `" alt="track-image">
+                        </div>
+                        <a href="" class="fav"><i class="far fa-heart"></i></a>
+                        <a href="" class="track-title">` + trackTitle + `</a>
+                        <div class="info-mobile">
+                            <a href="" class="track">` + trackTitle + `</a>
+                            <a href="" class="artist">` + trackArtist + `</a>
+                        </div>
+                        <p></p>
+                        <a href="" class="track-artist">` + trackArtist + `</a>
+                        <a href="" class="track-album">` + trackAlbum + `</a>
+                        <p class="duration">` + duration + `</p>
+                    </li>
+                    `
+                    document.querySelector(".ul").innerHTML += trackItem;
+
+                }
+
+            }
+        }
+    )
+
+
+
+    let trackItems = document.querySelectorAll(".track-item")
+
+    console.log(trackItems)
+    
+    for (i = 0; i < trackItems.length; i++) {
+        trackItems[i].addEventListener('mouseover', function() {
+            this.style.backgroundColor = "rgba(53, 47, 68, 0.692)";
+        })
+        trackItems[i].addEventListener('mouseout', function() {
+            this.style.backgroundColor = "";
+        })
+    }
+
+    let container = document.querySelectorAll(".img-container");
+        
+    console.log(container)
+    
+    for (i = 0; i < container.length; i++) {
+        
+        console.log(container[i])
+        container[i].addEventListener('mouseover', function() {
+            console.log("anda")
+            this.innerHTML = '<a href="index.html" class="play"><i class="fas fa-play-circle"></i></a>'
+        })
+        container[i].addEventListener('mouseout', function() {
+            console.log("anda")
+            this.innerHTML = '<img class="track-img" src="media/trackList/song-2.jpg" alt="track-image">'
+        })
+        
+    }
+
+    fetch
+})
