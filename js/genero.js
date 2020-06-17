93ad7bd07fe63663c134e5ee92439cd80b506259
gaston-lm <gastonlm11lm@gmail.com>
2020-06-17 12:15:08 -0300

    genero window.load

--- js/genero.js
+++ js/genero.js
@@ -13,12 +13,10 @@ window.addEventListener("load", function() {
     let datos = new URLSearchParams(queryString);
     let idGenero = datos.get('id');
 
-let proxy = 'https://cors-anywhere.herokuapp.com/';
-let url = proxy + "https://api.deezer.com/genre/" + idGenero + "/artists"
+    let proxy = 'https://cors-anywhere.herokuapp.com/';
+    let url = proxy + "https://api.deezer.com/genre/" + idGenero + "/artists"
 
-
-
-fetch(url)
+    fetch(url)
     .then(function(response){
             return response.json();
         }
